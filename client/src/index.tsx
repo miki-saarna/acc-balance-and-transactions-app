812aa31b7c748cef65ebe845f1f93f7cd9a6d34d
Mikito Saarna <mikitosaarna@gmail.com>
2022-01-20 12:33:14 -0600

    Converted client index and app files to tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// unsure if I need Router...
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(

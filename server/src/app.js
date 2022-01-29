25dbe0d6818a803f92c7911671ca1cf89b69d5c7
Mikito Saarna <mikitosaarna@gmail.com>
2022-01-29 10:18:02 -0600

    Commented out AccessTokenDB. Need to reconfigure

--- server/src/app.js
+++ server/src/app.js
@@ -10,12 +10,13 @@ const errorHandler = require('./errors/errorHandler');
 app.use(cors());
 // CORS API access approval to requesting domain below
 app.use(function(req, res, next) {
-  // const allowedDomains = ["http://localhost:3000", "https://acc-balance-and-transactions-app.vercel.app"];
-  // const origin = req.headers.origin;
-  // if (!allowedDomains.includes(origin)) {
-  //   next({ status: 400, message: `Access to fetch at from origin ${origin} has been blocked by CORS policy.` })
-  // }
-  res.header("Access-Control-Allow-Origin", "https://acc-balance-and-transactions-app.vercel.app");
+  const allowedDomains = ["http://localhost:3000", "https://acc-balance-and-transactions-app.vercel.app"];
+  const origin = req.headers.origin;
+  if (!allowedDomains.includes(origin)) {
+    next({ status: 400, message: `Access to fetch at from origin ${origin} has been blocked by CORS policy.` })
+  }
+  // res.header("Access-Control-Allow-Origin", "https://acc-balance-and-transactions-app.vercel.app");
+  res.header("Access-Control-Allow-Origin", origin);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
